0b7d7a4ecd2a6ec57bbebb63aa00da1d4bfaae8b
rigal34 <SST.plomberie@laposte.net>
2025-05-03 23:14:06 +0100

    Docs: Met à jour README avec structure Actualités et précisions DI/ParamConverter

---
+++ .history/src/Controller/PostController_20250502161948.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostController extends AbstractController
+{
+    #[Route('/actualites', name: 'app_actu')]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => 'PostController',
+        ]);
+    }
+}
---
+++ .history/src/Controller/PostController_20250502201245.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostController extends AbstractController
+{
+    #[Route('/actualites', name: 'app_actu')]
+    public function actu(PostRepository $postRepository): Response
+    {
+        return $this->render('post/postindex.html.twig', [
+            'posts' => $postRepository->findBy([], ['createdAt' => 'DESC']),
+        ]);
+    }
+
+// ::::::::::CREATION POUR LE DETAIL DU POST ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    #[Route('/actualites/{id}', name: 'app_actu_detail')]
+    public function detail(Post $post): Response
+    {
+        return $this->render('post/detail.html.twig', [
+            'post' => $post
+        ]);
+    }
+}
